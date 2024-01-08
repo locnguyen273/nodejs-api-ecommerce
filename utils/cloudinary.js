1d3c318a05f025182e94e6b73d93ecb69eea6760
locnguyen273 <77155886+locnguyen273@users.noreply.github.com>
2024-01-08 15:25:11 +0700

    Sprint 1/maintain api mindx (#12)
    * [update] update route user
    
    * product
    
    * [update] update controller get list product
    
    * add vercel & cloudinary
    
    ---------
    
    Co-authored-by: Loc Nguyen Janeto <nguyentanloc@teams.airlab.dev>

--- utils/cloudinary.js
+++ utils/cloudinary.js
@@ -1,11 +1,17 @@
 require("dotenv").config();
-const cloudinary = require("cloudinary");
+const cloudinary = require("cloudinary").v2;
 
 cloudinary.config({
   cloud_name: process.env.CLOUD_NAME,
   api_key: process.env.API_KEY,
   api_secret: process.env.API_SECRET,
 });
+
+const opts = {
+  overwrite: true,
+  invalidate: true,
+  resource_type: "auto",
+}
 
 const cloudinaryUploadImg = async (fileToUploads) => {
   return new Promise((resolve) => {
@@ -41,4 +47,39 @@ const cloudinaryDeleteImg = async (fileToDelete) => {
   });
 };
 
-module.exports = { cloudinaryUploadImg, cloudinaryDeleteImg };
+const uploadImage = (image) => {
+  return new Promise((resolve, reject) => {
+    cloudinary.uploader.upload(image, opts, (errors, result) => {
+      if (result && result.secure_url) {
+        console.log(result.secure_url);
+        return resolve(result.secure_url);
+      }
+      console.log(errors.message);
+      return reject({ message: errors.message });
+    });
+  });
+};
+
+module.exports = (image) => {
+  return new Promise((resolve, reject) => {
+    cloudinary.uploader.upload(image, opts, (error, result) => {
+      if (result && result.secure_url) {
+        console.log(result.secure_url);
+        return resolve(result.secure_url);
+      }
+      console.log(error.message);
+      return reject({ message: error.message });
+    });
+  });
+};
+
+module.exports.uploadMultipleImages = (images) => {
+  return new Promise((resolve, reject) => {
+    const uploads = images.map((base) => uploadImage(base));
+    Promise.all(uploads)
+      .then((values) => resolve(values))
+      .catch((err) => reject(err));
+  });
+};
+
+module.exports = { cloudinaryUploadImg, cloudinaryDeleteImg, uploadImage };
