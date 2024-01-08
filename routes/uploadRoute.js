d52cf32c6631f47b66cef830b8021f932fc0c5fc
Loc Nguyen Janeto <nguyentanloc@teams.airlab.dev>
2024-01-08 15:24:36 +0700

    add vercel & cloudinary

--- routes/uploadRoute.js
+++ routes/uploadRoute.js
@@ -1,7 +1,11 @@
 const express = require("express");
-const { uploadImages, deleteImages } = require("../controllers/uploadController");
+const {
+  uploadImages,
+  deleteImages,
+} = require("../controllers/uploadController");
 const { isAdmin, authMiddleware } = require("../middlewares/authMiddleware");
 const { uploadPhoto, productImgResize } = require("../middlewares/uploadImage");
+const { uploadImage } = require("../utils/cloudinary");
 const router = express.Router();
 
 router.post(
@@ -13,5 +17,18 @@ router.post(
   uploadImages
 );
 router.delete("/delete-img/:id", authMiddleware, isAdmin, deleteImages);
+
+router.post("/uploadImage", (req, res) => {
+  uploadImage(req.body.image)
+    .then((url) => res.send(url))
+    .catch((err) => res.status(500).send(err));
+});
+
+router.post("/uploadMultipleImages", (req, res) => {
+  uploadImage
+    .uploadMultipleImages(req.body.images)
+    .then((urls) => res.send(urls))
+    .catch((err) => res.status(500).send(err));
+});
 
 module.exports = router;
